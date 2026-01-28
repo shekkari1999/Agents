ec96f6b2b963a2714c901a29553e47a36a1af53c
shekkari21 <akhil.masters21@gmail.com>
2026-01-28 10:10:15 -0500

    started AI agents from scratch

--- scratch_agents/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from .search_web import search_web
-from .wikipedia import search_wikipedia, get_wikipedia_page
-from .calculator import calculator
-
-__all__ = ["search_web", "search_wikipedia", "get_wikipedia_page", "calculator"]
